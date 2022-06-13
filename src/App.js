e9a775f5defdb18ae29ab3bd13280482d9b55740
Sakshi <sakshikothawade3@gmail.com>
2022-06-14 00:15:59 +0530

    visualizer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+
+// imported and rendered in app.js
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisualizer></PathfindingVisualizer>
+    </div>
+  );
+}
+
+export default App;
